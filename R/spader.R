cdb457385e1659745cafa25a6e9fe010c4c00455
Jyunyao <asd435qwe45678@gmail.com>
2024-06-13 17:22:59 +0800

    240613

--- R/spader.R
+++ R/spader.R
@@ -844,7 +844,7 @@ SimilarityPair=function(data, datatype = c("abundance","incidence_freq", "incide
       colnames(x) <- c("Estimate", "s.e.", "95%.LCL", "95%.UCL") 
       return(x)
     })
-    z <- list("datatype"=type,"info"=info, "Empirical_absolute"=temp[[1]], "Empirical_relative"=temp[[3]], "Estimated_absolute"=temp[[2]], "Estimated_relative"=temp[[4]]) 
+    z <- list("datatype"=type,"info"=info, "Empirical_absolute"=temp[[1]], "Empirical_relative"=temp[[3]], "estimated_absolute"=temp[[2]], "estimated_relative"=temp[[4]]) 
     
   }  
   class(z) <- c("spadeTwo")
@@ -1195,7 +1195,7 @@ SimilarityMult=function(data,datatype=c("abundance","incidence_freq", "incidence
       rownames(Cqn_PC[[2]])=temp_PU
     }
     
-    z <- list("datatype"=datatype,"info"=info, "Empirical_absolute"=temp[[1]], "Empirical_relative"=temp[[4]],"Estimated_absolute"=temp[[2]], "Estimated_relative"=temp[[3]], "pairwise"=Cqn_PC, "similarity.matrix"=C_SM, "goal"=method, "q"=q)
+    z <- list("datatype"=datatype,"info"=info, "Empirical_absolute"=temp[[1]], "Empirical_relative"=temp[[4]],"estimated_absolute"=temp[[2]], "estimated_relative"=temp[[3]], "pairwise"=Cqn_PC, "similarity.matrix"=C_SM, "goal"=method, "q"=q)
   }
   if(datatype == "incidence_raw"){
     data <- X
@@ -1475,7 +1475,7 @@ SimilarityMult=function(data,datatype=c("abundance","incidence_freq", "incidence
       rownames(Cqn_PC[[2]])=temp_PU
     }
     
-    z <- list("datatype"=datatype,"info"=info, "Empirical_absolute"=temp[[1]], "Empirical_relative"=temp[[4]],"Estimated_absolute"=temp[[2]], "Estimated_relative"=temp[[3]], "pairwise"=Cqn_PC, "similarity.matrix"=C_SM, "goal"=method, "q"=q)
+    z <- list("datatype"=datatype,"info"=info, "Empirical_absolute"=temp[[1]], "Empirical_relative"=temp[[4]],"estimated_absolute"=temp[[2]], "estimated_relative"=temp[[3]], "pairwise"=Cqn_PC, "similarity.matrix"=C_SM, "goal"=method, "q"=q)
   }
   class(z) <- c("spadeMult")
   z
